f401f42704dd6da0fb5ccc02250dd7b53a09a48a
unknown <ortizmoncada2006@gmail.com>
2025-05-03 14:55:55 -0500

    admin y arreglos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,15 +3,17 @@ import Cart from "../views/CART.vue"
 import Seeproduct from "../views/SEEPRODUCT.vue"
 import Admin from "../views/Admin.vue"
 import {createRouter, createWebHashHistory} from "vue-router"
+import PaymentGateway from "../views/PAYMENT GATEWAY.vue"
   
-  const routes = [
-    { path: '/', component: Home },
-    { path: '/cart', component: Cart },
-    { path: '/seeproduct', component: Seeproduct}, 
-    { path: '/admin', component: Admin},
-  ]
+const routes = [
+  { path: '/', component: Home },
+  { path: '/cart', component: Cart },
+  { path: '/seeproduct', component: Seeproduct}, 
+  { path: '/admin', component: Admin},
+  { path: '/gateway', component: PaymentGateway},
+]
   
-  export const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-  })
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})

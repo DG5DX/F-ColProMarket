abbc1b000816d5db51435bb7253e2ded1f2bbce2
Andres sierra garcia <andresfelipe3666@gmail.com>
2025-05-11 18:41:45 -0500

    feat: update axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:3999/api",
+    /* baseURL: "http://localhost:3999/api */
+    baseURL: "https://backend-proyectofinal-vrso.onrender.com/api"
 });
 
 client.interceptors.request.use(

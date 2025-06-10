1336f0d27666083ff612cc16b20b7f088ee1244f
LaPulga1828 <rodrigueztascojoseluis18@gmail.com>
2025-06-10 11:14:55 -0500

    Arreglo de css y loading en home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,43 +33,67 @@
     </q-carousel>
   </div>
   <div class="productos-wrapper">
-  <div class="productos row q-gutter-xl justify-start q-pa-xl">
-    <div 
-      v-for="(producto, index) in productos"
-      :key="index"
-      class="my-card card"
-      @click="verDetalleProducto(producto)"
-    >
-      <div class="card-img">
-        <div class="img">
-          <q-img
-            :src="producto.images[0].urlImage"
-            :alt="producto.name"
-            fit="cover"
-          >
-            <template v-slot:loading>
-              <q-spinner color="primary" />
-            </template>
-          </q-img>
+    <!-- Skeleton Loader -->
+    <div v-if="productos.length === 0" class="productos row q-gutter-xl justify-start q-pa-xl">
+      <div 
+        v-for="n in 8" 
+        :key="'skeleton-'+n"
+        class="my-card card skeleton-card"
+      >
+        <div class="card-img skeleton">
+          <div class="img skeleton"></div>
+        </div>
+        <div class="card-title skeleton"></div>
+        <div class="card-subtitle skeleton"></div>
+        <hr class="card-divider">
+        <div class="card-footer">
+          <div class="card-price skeleton"></div>
+          <button class="card-btn skeleton"></button>
         </div>
       </div>
-      <div class="card-title">{{ producto.name }}</div>
-      <div class="card-subtitle">{{ producto.description }}</div>
-      <hr class="card-divider">
-      <div class="card-footer">
-        <div class="card-price"><span>$</span> {{ producto.price.toFixed(2) }}</div>
-        <button class="card-btn" @click.stop="addToTheCart(producto)">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path>
-            <path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path>
-            <path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path>
-            <path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path>
-          </svg>
-        </button>
+    </div>
+
+    <!-- Productos reales -->
+    <div 
+      v-else
+      class="productos row q-gutter-xl justify-start q-pa-xl"
+    >
+      <div 
+        v-for="(producto, index) in productos"
+        :key="index"
+        class="my-card card"
+        @click="verDetalleProducto(producto)"
+      >
+        <div class="card-img">
+          <div class="img">
+            <q-img
+              :src="producto.images[0].urlImage"
+              :alt="producto.name"
+              fit="cover"
+            >
+              <template v-slot:loading>
+                <q-spinner color="primary" />
+              </template>
+            </q-img>
+          </div>
+        </div>
+        <div class="card-title">{{ producto.name }}</div>
+        <div class="card-subtitle">{{ producto.description }}</div>
+        <hr class="card-divider">
+        <div class="card-footer">
+          <div class="card-price"><span>$</span> {{ producto.price.toFixed(2) }}</div>
+          <button class="card-btn" @click.stop="addToTheCart(producto)">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path>
+              <path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path>
+              <path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path>
+              <path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path>
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   </div>
-</div>
       </div>
     </div>
     
@@ -249,7 +273,7 @@ const imagenesCarrusel = ref([
     alt: "Phantom HD"
   },
   {
-    src: new URL('../assets/Bryan.jpeg', import.meta.url).href,
+    src: "https://img.fcbayern.com/image/upload/f_auto,q_auto,w_1280/eCommerce/produkte/51116",
     alt: "Product Image"
   }
 ]);

66922fcea1044d859a1eb37ec4a17c2bf84250ea
Andres sierra garcia <andresfelipe3666@gmail.com>
2025-06-13 17:12:48 -0500

    update all

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,6 @@ import Admin from "../views/Admin.vue"
 import AdminCategory from "../views/AdminCategory.vue"
 import AdminUsers from "../views/AdminUsers.vue"
 import userProfile from "../views/userProfile.vue"
-import invoice from "../components/invoice.vue"
 import Contact from "../views/Contact.vue"
 import changePassword from "../components/changePassword.vue"
 import AdminSales from "../views/AdminSales.vue"
@@ -26,7 +25,6 @@ const routes = [
   { path: '/gateway', component: PaymentGateway },
   { path: '/search', component: Search },
   { path: '/sales', component: sales },
-  { path:'/invoice' , component:invoice},
   { path:'/contact', component: Contact},
   { path:'/favorite',component: Favorite},
   {path:'/userProfile', component:userProfile},
